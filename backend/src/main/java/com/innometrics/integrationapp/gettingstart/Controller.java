306edf9347928ef6a0494c39d6a93301d8918a79
killpack <killpackoff@gmail.com>
2015-01-14 18:39:07 +0400

    refs #7032 Java getting started

--- backend/src/main/java/com/innometrics/integrationapp/gettingstart/Controller.java
+++ backend/src/main/java/com/innometrics/integrationapp/gettingstart/Controller.java
@@ -2,6 +2,7 @@ package com.innometrics.integrationapp.gettingstart;
 
 import com.innometrics.utils.app.commons.settings.exception.IncorrectConfigurationException;
 import com.innometrics.utils.app.commons.settings.store.ProfileCloudSettings;
+import com.innometrics.utils.app.commons.settings.strategy.OnlyEnvironmentStrategy;
 import com.innometrics.utils.app.commons.settings.wrapper.ProfileCloudWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -26,7 +27,7 @@ public class Controller {
     @RequestMapping(value = "/view")
     public String view(Model model) {
         try {
-            model.addAttribute("profileCloudDAO",new ProfileCloudSettings());
+            model.addAttribute("profileCloudDAO",new ProfileCloudSettings(new OnlyEnvironmentStrategy()));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (IncorrectConfigurationException e) {
