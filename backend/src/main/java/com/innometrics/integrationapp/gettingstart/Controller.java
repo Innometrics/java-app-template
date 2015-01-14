43a141054442cb9d5cab4d798886fe0825962782
killpack <killpackoff@gmail.com>
2015-01-14 16:44:18 +0400

    refs #7032 Java getting started

--- backend/src/main/java/com/innometrics/integrationapp/gettingstart/Controller.java
+++ backend/src/main/java/com/innometrics/integrationapp/gettingstart/Controller.java
@@ -1,19 +1,32 @@
 package com.innometrics.integrationapp.gettingstart;
 
-import com.innometrics.utils.app.commons.settings.wrapper.ProfileCloudWrapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by killpack on 13.01.15.
  */
 @org.springframework.stereotype.Controller
 public class Controller {
-
+//    @Autowired
+//    private ProfileCloudWrapper profileCloudDAO;
 
 
     @RequestMapping(value = "/" )
     public String root() {
+//        try {
+//            profileCloudDAO.getApp();
+//        } catch (NoPropertyAvailableException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
         return "test";
     }
 
